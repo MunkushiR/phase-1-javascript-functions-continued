659ed5c699cacd6e57b6a71815bd1f7e556a3a9d
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-03-16 12:45:34 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(saturday){
+    return "This Saturday, I want to roller-skate!"
+    }
+    function mondayWork(Monday){
+        return "This Monday, I want to bathe my dog!"
+        }function mondayWork(Monday){
+            return "This Monday, I will go to the office."
+            }
+        function mondayWork(Monday){
+            return "This Monday, I will work from home."
+            }
+    function wrapAdjective(){
+        let result = wrapAdjective(" *")
+        let emphatic = result("a hard worker")
+        emphatic ="you are *a hard worker*!"
+    }
+    function wrapAdjective(){
+        let result = wrapAdjective(" ||")
+        let emphatic = result("a dedicated programmer")
+        emphatic ="you are ||a dedicated programmer||!"
+    }
